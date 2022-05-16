7b8730a9489f6ffec3753d4cee6118524c1af6b7
Harald Schwab <hs@rigbit.at>
2022-05-16 22:45:15 +0200

    quick implementation for pwa-save

--- src/app/routes/v1/pwa/pwa.router.ts
+++ src/app/routes/v1/pwa/pwa.router.ts
@@ -7,6 +7,10 @@ import { errorResponse, ResponseError } from '@util/response';
 import { Serializable } from '@util/serialize';
 import { createZip } from '@util/zip';
 import { Request, Response } from 'express';
+import { v4 } from 'uuid';
+import path from 'path';
+import { PATHS } from '@config/environment';
+import * as fs from 'fs';
 
 export enum Place {
   generated = 'generated',
@@ -31,11 +35,13 @@ export class PwaRouter extends BaseRouter {
 
     if (zip) { return res.download(zip.absolutePath); }
     await errorResponse(req, res,
-      [new ResponseError(RESPONSE_CODES.NOT_FOUND, `could not find pwa with name [${req.params.key}]`)],
+      [ new ResponseError(RESPONSE_CODES.NOT_FOUND, `could not find pwa with name [${req.params.key}]`) ],
       RESPONSE_CODES.NOT_FOUND);
   }
 
   protected async post(req: Request, res: Response): Promise<void> {
+    const uuid = v4();
+    const base_href = path.join('/', 'public', uuid, '/');
     const pwa = PwaRouter.getFileFrom(Place.template);
     const compiled = pwa?.children.map(function f(file: File): Serializable<SerializedFile> {
       if (file?.isDirectory()) {
@@ -44,7 +50,7 @@ export class PwaRouter extends BaseRouter {
         return copy;
       }
       if (TEMPLATE_EXT_REGEXP.test(file.ext || '')) {
-        return new TemplateFile(file, req.body);
+        return new TemplateFile(file, { base_href, ...req.body });
       }
       return file;
     }).map(m => m.serialize(true));
@@ -53,12 +59,27 @@ export class PwaRouter extends BaseRouter {
       const image = req.body.pwa_image ? Buffer.from(req.body.pwa_image, 'base64') : undefined;
       compiled.push(await getPwaImages(image));
 
+      await save2Public(compiled, path.join(PATHS.PUBLIC, uuid));
       await createZip(compiled, `pwa/${req.body.pwa_title}.zip`);
 
       res.json(compiled);
     } else {
       await errorResponse(req, res,
-        [new ResponseError(RESPONSE_CODES.SERVER_ERROR, 'could not create pwa')], RESPONSE_CODES.SERVER_ERROR);
+        [ new ResponseError(RESPONSE_CODES.SERVER_ERROR, 'could not create pwa') ], RESPONSE_CODES.SERVER_ERROR);
     }
   }
+}
+
+
+async function save2Public(files: SerializedFile[], rootPath: string): Promise<void> {
+  fs.mkdirSync(rootPath);
+  files.forEach((file) => {
+    const filePath = path.join(rootPath, file.name);
+    if (file.children) {
+      save2Public(file.children, filePath);
+    } else {
+      const type = file.ext.match(/(png|jpe?g)/i) ? 'base64' : 'utf-8';
+      fs.writeFileSync(filePath, file.content, type);
+    }
+  });
 }
